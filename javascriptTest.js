ee4cb321d6315b43e24e9a07b468e1385746ff7c
Luke <kilbourneluke138@gmail.com>
2023-02-10 12:31:18 +0000

    first commit

---
+++ javascriptTest.js
@@ -0,0 +1,4 @@
+const months  =['Jan', 'Feb', 'March']
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, months[random])
---
+++ script.js
@@ -0,0 +1,5 @@
+let inspirationalMessages = ['It Always Seems Impossible Until Its Done. - Nelson Mandela', 'Out Of The Mountain Of Despair, A Stone Of Hope. - Martin Luther King', 'Don\'t Let Yesteray Take Up Too Much Of Today. - Will Rogers', 'Open Your Mind. Get Up Off The Couch. Move! - Anthony Bourdain'];
+
+const random = Math.floor(Math.random() * inspirationalMessages.length);
+console.log(random, inspirationalMessages[random])
+
